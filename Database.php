da7a9e7a8230888e0858d6d5d333908321f3e874
Ritika Ghosh <104667333+ritikaghosh0402@users.noreply.github.com>
2023-07-15 21:46:57 +0530

    Update code

--- Database.php
+++ Database.php
@@ -6,7 +6,4 @@
     $con=new mysqli($host, $username, $password, $database);
     if($con->connect_error){
         die("Error:".$con->connect_error);}
-    // else{
-    //     echo "File connected to Database -> TOPSTACK"."<br>";
-    // }
 ?>
